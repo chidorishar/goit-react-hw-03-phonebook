3c6ebe7560008645ac365e64cbd50082d7f241da
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-11-07 15:53:25 +0200

    Changes from previous HW

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,40 +1,20 @@
 import { PropTypes } from 'prop-types';
-import { Box } from 'components/common/Box/Box.styled';
-import { theme } from 'utils/theme';
-import { ContactInfo, ContactsList, DeleteButton } from './ContactList.styled';
+
+import { ContactListItem } from './ContactListItem/ContactListItem';
+
+import { ContactsList } from './ContactList.styled';
 
 export function ContactList(props) {
   const { contacts, onContactRemoveCallback } = props;
 
   return (
     <ContactsList>
-      {contacts.map(({ name, number, id }) => (
-        <Box
-          mb={[2]}
-          p={[2]}
-          display="flex"
-          alignItems="center"
-          justifyContent="space-between"
-          border="small"
-          borderRadius="big"
-          borderColor="#00000039"
-          style={{
-            boxShadow: theme.shadows.inputInset,
-          }}
-          transition="box-shadow normal"
-          as="li"
-          key={id}
-        >
-          <ContactInfo>
-            {name}: {number}
-          </ContactInfo>
-          <DeleteButton
-            onClick={() => onContactRemoveCallback(id)}
-            isDelete={true}
-          >
-            Delete
-          </DeleteButton>
-        </Box>
+      {contacts.map(contactData => (
+        <ContactListItem
+          key={contactData.id}
+          contactData={contactData}
+          onContactRemoveCallback={onContactRemoveCallback}
+        />
       ))}
     </ContactsList>
   );
